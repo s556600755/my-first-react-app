66a9acf3750118b3d03fb65be18ed54876387c0a
wenjie <s95175388@gmail.com>
2026-06-10 13:50:16 +0800

    環境建置與首個組件

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  const name = "未來的前端工程師"; // 我們定義一個變數
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>恭喜！這是我的第一個 React 專案 🎉</h1>
+      <p>嗨，{name}，今天是 30 天計畫的第一天！</p>
+    </div>
+  );
+}
+
+export default App;
