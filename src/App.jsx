4bf82c1b3c2e54ad47a963e1cde3a37536224ccf
wenjie <s95175388@gmail.com>
2026-06-10 14:11:39 +0800

    完成day3 成功實作 useState 數字計數器

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import UserCard from './UserCard';
+import { useState } from 'react';
 function App() {
-
+  const [count, setCount] = useState(0);
+  function handleAdd() {
+    setCount(count + 1); // 告訴 React：請幫我把 count 變成現在的值加 1
+  }
+  function handleIncrease() {
+    setCount(count - 1)
+  }
+  function reset(){
+    setCount(0)
+  }
   return (
-    <div style={{ padding: '20px', maxWidth: '400px', margin: '0 auto' }}>
-      <h1>工程團隊成員名單 👥</h1>
+    <div style={{ padding: '30px', textAlign: 'center' }}>
+      <h1>Day 3：useState 體驗 🌟</h1>
 
-      {/* 2. 重複使用組件，並透過自訂屬性（Props）把資料傳進去 */}
-      <UserCard name="小明" job="前端實習生" />
-      <UserCard name="阿華" job="資深前端工程師" />
-      <UserCard name="你" job="未來的前端架構師" />
+      {/* 4. 這裡會動態顯示目前的 count 值 */}
+      <h2 style={{ fontSize: '48px' }}>{count}</h2>
+
+      {/* 5. 綁定點擊事件 onClick，當按鈕被點擊時，執行 handleAdd 函式 */}
+      <button
+        onClick={handleAdd}
+        style={{ padding: '10px 20px', fontSize: '18px', cursor: 'pointer' }}
+      >
+        點我加 1
+      </button>
+      <button
+        onClick={handleIncrease}
+        style={{ padding: '10px 20px', fontSize: '18px', cursor: 'pointer' }}
+      >
+        點我減 1
+      </button>
+      <button
+        onClick={reset}
+        style={{ padding: '10px 20px', fontSize: '18px', cursor: 'pointer' }}
+      >
+        點我歸零
+      </button>
     </div>
   );
 }
